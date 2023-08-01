cf701c58df764bd6ed6002ec9d6d4a13cbc4f9cf
phsfxx <phsilveiraphsf@gmail.com>
2023-08-01 19:05:15 -0300

    Mensagem do commit aqui

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     img.setAttribute("src", "assets/avatar.png.light")
   } else {
-    img.setAttribute("src", "assets/FB_IMG_1685137557924.jpg")
+    img.setAttribute("src", "assets/avatar.jpg.dark")
   }
 }
 assets / avatar.png.light
